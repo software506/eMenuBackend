4a182e241d336f265c6896e8425bc96f83363ff9
苏赵勇 <sutaojie@gmail.com>
2020-03-08 17:19:33 +0800

    post

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.set('Content-Type', 'application/json; charset=utf-8')
   res.send(JSON.stringify(require('../mock/main/main')));
 });
